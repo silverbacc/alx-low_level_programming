3c0c44cd9c4e012a577b0f19fa9ac6bdcd3836ba
silverbacc <chiboyboxx@outlook.com>
2022-03-17 19:29:26 -0700

    1-isdigit.c

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,37 +1,18 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
- * print_remaining_days - takes a date and prints how many days are
- * left in the year, taking leap years into account
- * @month: month in number format
- * @day: day of month
- * @year: year
- * Return: void
+ * _isdigit - checks if parameter is a number between 0 to 9.
+ * @c: input number.
+ * Return: 1 if is a number (0 to 9), 0 in other case.
  */
-
-void print_remaining_days(int month, int day, int year)
+int _isdigit(int c)
 {
-	if (year % 4 == 0 || ((year % 100 == 0) && (year % 400 == 0)))
+	if (c >= 48 && c <= 57)
 	{
-		if (month > 2 && day >= 60)
-		{
-			day++;
-		}
-
-		printf("Day of the year: %d\n", day);
-		printf("Remaining days: %d\n", 366 - day);
+		return (1);
 	}
 	else
 	{
-		if (month == 2 && day == 60)
-		{
-			printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
-		}
-		else
-		{
-			printf("Day of the year: %d\n", day);
-			printf("Remaining days: %d\n", 365 - day);
-		}
+		return (0);
 	}
 }
